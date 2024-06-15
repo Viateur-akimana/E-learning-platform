d5bef969b20e21f1e45a26213f4a8db365515b64
viateur <akimanaviateur94@gmail.com>
2024-06-15 21:44:15 +0200

    Making user model

--- src/main/java/org/example/restapi/model/User.java
+++ src/main/java/org/example/restapi/model/User.java
@@ -1,32 +1,32 @@
-//package org.example.restapi.model;
-//
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@Entity
-//@Table(name = "users")
-//
-//public class User {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//    private String name;
-//    private String email;
-//    private String password;
-//    private String roles;
-//
-//    public User() {
-//    }
-//
-//    public User(int id, String name, String email, String password, String roles) {
-//        this.id = id;
-//        this.name = name;
-//        this.email = email;
-//        this.password = password;
-//        this.roles = roles;
-//    }
-//}
+package org.example.restapi.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String roles;
+
+    public User() {
+    }
+
+    public User(int id, String name, String email, String password, String roles) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+}
